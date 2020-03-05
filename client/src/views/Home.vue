4dc466f0d64d7c824c7f9228aa71922854428591
Yoichi Fujimoto <wozozo@gmail.com>
2020-03-05 17:59:45 +0900

    chore: gomi

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -21,9 +21,6 @@ import Component from "vue-class-component";
 @Component
 export default class Home extends Vue {
   words = "";
-  get form() {
-    return this.$form.createForm(this, { name: "horizontal_login" });
-  }
 
   get apiHost() {
     return process.env.VUE_APP_API_HOST || "";
@@ -43,40 +40,3 @@ export default class Home extends Vue {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.form {
-  margin: 0 auto;
-  max-width: 320px;
-  button {
-    background-image: linear-gradient(to left, #fff, #f2f2f2);
-    border: 1px solid #e3e3e3;
-    border-radius: 3px;
-    width: 63px;
-    height: 34px;
-    &:hover {
-      border-color: #bdbdbd;
-    }
-  }
-}
-
-.word-input {
-  background: #fff;
-  border-radius: 3px;
-  padding: 12px 8px 12px 18px;
-  margin-bottom: 15px;
-  position: relative;
-  box-shadow: 0 0 5px rgba(16, 14, 23, 0.25);
-}
-
-.form input {
-  border: 0;
-  font-family: 24px;
-  font-size: 16px;
-  width: 210px;
-  margin-right: 5px;
-  &:focus {
-    outline: none;
-  }
-}
-</style>
